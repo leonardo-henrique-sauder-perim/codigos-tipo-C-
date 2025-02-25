3f1bd571abbe03734089078899f706c290d4e8a5
leonardo-henrique-sauder-perim <159266822+leonardo-henrique-sauder-perim@users.noreply.github.com>
2025-02-25 11:49:03 -0300

    Add files via upload

---
+++ main texte 2(2).c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main () {
+    int idade; 
+          printf("digite idade");
+          scanf("%d",& idade);
+          
+          if(idade>=18){
+              printf("festa do job");
+          }
+          else if(idade>=12){
+              printf(" passar cinco minutos na festa");
+          }   
+          else if(idade<=11){
+              printf("chora");
+          }
+          
+             printf(" vai para casa dormir ");
+              return 0;
+}
+              
+              
+
+          
+
